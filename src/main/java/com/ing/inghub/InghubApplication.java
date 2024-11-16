ce697169a7163c6ee257f3b937b3d3a2fc8f6a18
dilşad karakuş <dilsadkarakus@dilsad-MacBook-Air.local>
2024-11-16 14:33:40 +0300

    initial commit

---
+++ src/main/java/com/ing/inghub/InghubApplication.java
@@ -0,0 +1,13 @@
+package com.ing.inghub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InghubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InghubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ing/inghub/InghubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ing.inghub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InghubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
